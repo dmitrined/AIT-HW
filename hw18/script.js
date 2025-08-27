e1e235a3be9672c0a03ca06451200c4ca187703f
Dmitri Nedioglo <dm.nedioglo@gmail.com>
2025-08-27 21:11:01 +0200

    ok

--- hw18/script.js
+++ hw18/script.js
@@ -4,7 +4,6 @@ const userList = document.querySelector('.user-list');
 
 btn.addEventListener('click', (e) => {
     e.preventDefault();
-
     const firstName = form.first_name.value;
     const lastName = form.last_name.value;
     const email = form.email.value;
@@ -13,21 +12,15 @@ btn.addEventListener('click', (e) => {
         alert('Пожалуйста, заполните все поля.');
         return;
     }
-    // Создаем новый элемент списка (<li>)
     const li = document.createElement('li');
     li.textContent = `Name: ${firstName} ${lastName}, Email: ${email}`;
-    // Создаем кнопку "Удалить"
     const deleteBtn = document.createElement('button');
     deleteBtn.textContent = 'Удалить';
     deleteBtn.classList.add('delete-btn'); 
-    // Добавляем обработчик клика на кнопку "Удалить"
     deleteBtn.addEventListener('click', () => {
         li.remove();
     });
-    // Добавляем кнопку к элементу списка
     li.appendChild(deleteBtn);
-    // Добавляем элемент списка в ul
     userList.appendChild(li);
-    // Сбрасываем значения полей формы
     form.reset();
 });
